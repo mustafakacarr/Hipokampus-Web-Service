19f4aa0caa6b7f6b71e5a89e4ade6378f0de4d5a
dezeve <dezeve@outlook.com>
2024-03-18 02:39:24 +0300

    feat: add payment checkout card

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,13 +1,46 @@
 import React from "react"
 import UserPanelLayout from "../layouts/UserPanelLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreditCard, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const Payment = () => {
     return (
         <UserPanelLayout>
             <div className="mx-4 px-4 mb-5 mt-2 row gx-0">
-                <div className="col-sm-12 col-md-8 col-lg-9">
+                <div className="col-sm-12 col-md-8 col-xxl-9">
                 </div>
-                <div className="col-sm-12 col-md-4 col-lg-3">
+                <div className="col-sm-12 col-md-4 col-xxl-3">
+                    <div className="card text-center">
+                        <div className="card-header fs-4 fw-bold p-3">
+                            <span className="me-2"><FontAwesomeIcon icon={faCreditCard} /></span>
+                            Checkout
+                        </div>
+                        <div className="card-body text-start payment-card-body my-1">
+                            <h5 className="card-title fw-bold mb-2 text-primary mb-3 ms-2 mt-1">Your Purchases</h5>
+                            <ul class="list-group list-group-flush text-start">
+                                {(() => {
+                                    let purchases = [];
+                                    for (let index = 1; index < 5; index++) {
+                                        purchases.push(
+                                            <li className="list-group-item d-flex justify-content-between" key={index}>
+                                                <span>Cafe item {index}</span>
+                                                <span>x{index}</span>
+                                                <span>{index}₺</span>
+                                                <FontAwesomeIcon className="text-primary" icon={faTrash} />
+                                            </li>
+                                        );
+                                    }
+                                    return purchases;
+                                })()}
+                            </ul>
+                        </div>
+                        <div className="card-footer fw-bold p-3">
+                            <div className="mx-2 d-flex justify-content-between">
+                                <span>Total</span>
+                                <span>200₺</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </UserPanelLayout>
