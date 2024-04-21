9d69b9ad0e42a31c3b45845290795614f8b5e0b9
dezeve <dezeve@outlook.com>
2024-04-21 20:33:07 +0300

    refactor: refactor cards in benefits page

--- src/components/benefits-components/BenefitsCard.jsx
+++ src/components/benefits-components/BenefitsCard.jsx
@@ -4,14 +4,14 @@ const BenefitsCard = ({ data, index }) => {
 
     return (
         <div key={index} className="px-1 px-lg-2 px-lg-3 col-12 col-md-6 col-xl-4 mb-4 mb-md-3">
-            <div className="card benefits-card text-dark">
+            <div className="card benefits-card">
                 <img src="https://hipokampus.com.tr/images/kampanya/en/hipokampus-discount-coupon-fizyoform-partner.jpg"
                     alt="Fizyoform Discount Image" className="benefits-card-image" />
                 <div className="card-img-overlay benefits-card-overlay">
-                    <h4 className="card-title mt-2 text-light fw-bold text-center">
+                    <h4 className="card-title mt-2 text-primary fw-bold text-center">
                         {data.title}
                     </h4>
-                    <p className="card-text mt-4 fw-bold benefits-card-description">{data.description}</p>
+                    <p className="card-text mt-4 fw-bold benefits-card-description text-secondary">{data.description}</p>
                 </div>
             </div>
         </div>
