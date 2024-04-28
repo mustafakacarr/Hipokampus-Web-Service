c3f4a7d0f735873bfb39c53b61c31a042ade157c
dezeve <dezeve@outlook.com>
2024-04-28 15:11:08 +0300

    feat: redesign profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,78 +5,48 @@ import PhoneInput from "../components/phone-input-component/PhoneInput"
 const Profile = () => {
   return (
     <UserPanelLayout>
-      <div className="main mt-3">
-        <div className="row">
-          <div className="col-12 col-sm-12 col-md-6 col-lg-8">
-            <div className="card">
-              <div className="card-body">
-                <div className="col-12">
-                  <h3>Update Profile</h3>
-                  <div className="row">
-                    <div className="col-12">
-                      <div className="mb-3">
-                        <label className="form-label">Username</label>
-                        <input type="text" className="form-control" disabled />
-                        {/* Cannot be changed */}
-                      </div>
-                    </div>
-                    <div className="col-6">
-                      <div className="mb-3">
-                        <label className="form-label">First Name</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-6">
-                      <div className="mb-3">
-                        <label className="form-label">Last Name</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="mb-3">
-                        <label className="form-label">Email</label>
-                        <input type="text" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="mb-3">
-                        <PhoneInput />
-                      </div>
-                    </div>
-                    <div className="col-12 btn btn-success">Save</div>
-                  </div>
-                </div>
-              </div>
+      <div className="mt-3 mx-5 mx-md-3 mx-lg-5">
+        <div className="border rounded mx-auto py-1 px-3" style={{ maxWidth: "1100px", backgroundColor: "#fafafa" }}>
+          <div className="row gx-2 mt-3">
+            <div className="col-12 col-md-6 col-lg-3 py-1">
+              <button className="btn btn-dark rounded px-3 w-100 h-100" type="button">Personal Information</button>
+            </div>
+            <div className="col-12 col-md-6 col-lg-3 py-1">
+              <button className="btn btn-dark rounded px-3 w-100 h-100" type="button">Invoice Details</button>
+            </div>
+            <div className="col-12 col-md-6 col-lg-3 py-1">
+              <button className="btn btn-dark rounded px-3 w-100 h-100" type="button">Payment History</button>
+            </div>
+            <div className="col-12 col-md-6 col-lg-3 py-1">
+              <button className="btn btn-dark rounded px-3 w-100 h-100" type="button">Account History</button>
             </div>
           </div>
-          <div className="col-12 col-sm-8 col-md-6 col-lg-4">
-            <div className="card">
-              <div className="card-body p-3">
-                <div>
-                  <h3>Change Password</h3>
-                  <div className="row">
-                    <div className="col-12">
-                      <div className="mb-3">
-                        <label className="form-label">Old Password</label>
-                        <input type="password" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="mb-3">
-                        <label className="form-label">New Password</label>
-                        <input type="password" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-12">
-                      <div className="mb-3">
-                        <label className="form-label">New Password Again</label>
-                        <input type="password" className="form-control" />
-                      </div>
-                    </div>
-                    <div className="col-12 btn btn-success">Update</div>
-                  </div>
-                </div>
-              </div>
+          <div className="row gx-2 mt-4 mb-5">
+            <div className="col-12 py-1 mb-2">
+              <h3 className="fw-bold">Update Profile</h3>
+            </div>
+            <div className="col-12 py-1">
+              <label className="form-label">Username</label>
+              <input type="text" className="form-control" disabled />
+            </div>
+            <div className="col-12 col-md-6 py-1">
+              <label className="form-label">First Name</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="col-12 col-md-6 py-1">
+              <label className="form-label">Last Name</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="col-12 col-md-6 py-1">
+              <label className="form-label">Email</label>
+              <input type="text" className="form-control" />
+            </div>
+            <div className="col-12 col-md-6 py-1">
+              <label className="form-label">Phone Number</label>
+              <PhoneInput />
+            </div>
+            <div className="mt-4 col-12 col-md-4 col-lg-3 col-xl-2 mx-auto text-center">
+              <button className="btn btn-primary w-100">Update</button>
             </div>
           </div>
         </div>
