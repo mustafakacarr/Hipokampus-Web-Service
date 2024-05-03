8a4cce75675f6449361bcf1de597f857b71d6a39
dezeve <dezeve@outlook.com>
2024-05-03 18:22:53 +0300

    feat: add bypass for forms without extra steps

--- src/components/order-components/OrderSteps.jsx
+++ src/components/order-components/OrderSteps.jsx
@@ -5,7 +5,8 @@ import OrderFailed from "./OrderFailed";
 import OrderSuccess from "./OrderSuccess";
 import MeetingRoomOrderSteps from "./services-order-steps/MeetingRoomOrderSteps";
 import Done from "./forms/Done";
-import { MEETING_ROOM_ORDER, VIRTUAL_OFFICE_ORDER, CATERING_ORDER, CAFE_ORDER } from "../../constants/OrderType";
+import { MEETING_ROOM_ORDER, VIRTUAL_OFFICE_ORDER, CATERING_ORDER, CAFE_ORDER, TICKET_ORDER, COWORKING_SPACE_ORDER }
+  from "../../constants/OrderType";
 import VirtualOfficeOrderSteps from "./services-order-steps/VirtualOfficeOrderSteps";
 import CateringOrderSteps from "./services-order-steps/CateringOrderSteps"
 import InvoiceInformationForm from "./forms/InvoiceInformationForm";
@@ -14,7 +15,7 @@ import CafeOrderSteps from "./services-order-steps/CafeOrderSteps";
 const firstSteps = [
   {
     label: "Personal Info",
-    path: "/order/personal-info",
+    path: "/order/invoice-information",
     component: <InvoiceInformationForm />,
   }
 ];
@@ -33,6 +34,15 @@ const getCustomStep = (orderType) => {
       break;
     case CAFE_ORDER:
       orderCustomSteps = CafeOrderSteps;
+      break;
+    case TICKET_ORDER:
+      {/* There is no extra step(s) for Ticket Order */ }
+      orderCustomSteps = false;
+      break;
+    case COWORKING_SPACE_ORDER:
+      {/* There is no extra step(s) for Coworking Space Order */ }
+      orderCustomSteps = false;
+      break;
     default:
       break;
   }
@@ -54,6 +64,10 @@ const lastSteps = [
   },
 ];
 
-const OrderSteps = firstSteps.concat(customSteps, lastSteps);
+if (customSteps) {
+  var OrderSteps = firstSteps.concat(customSteps, lastSteps);
+} else {
+  var OrderSteps = firstSteps.concat(lastSteps);
+}
 
 export default OrderSteps;
