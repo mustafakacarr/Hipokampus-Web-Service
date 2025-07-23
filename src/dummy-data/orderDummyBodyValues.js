26daeb912e47bd91989235e06ae208684328bee8
Mustafa KAÇAR <mustafakacar48@outlook.com.tr>
2025-07-23 20:54:49 +0300

    Unused file deleted and dummy datas are supported by the comments to avoid from confusions

---
+++ src/dummy-data/orderDummyBodyValues.js
@@ -0,0 +1,30 @@
+const cafeOrder = {
+  //Cafe order mockup
+  userId: 1,
+  basket: [
+    { id: 1, quantity: 2 },
+    { id: 2, quantity: 1 },
+  ],
+  orderType,
+};
+const cateringOrder = {
+  //Cafe order mockup
+  userId: 1,
+  priGroupFoodId: 1,
+  secGroupFoodId: 1,
+  thirdGroupFoodId: 1,
+};
+
+const packageOrder = {
+  userId: 1,
+  packageId: 1,
+  periodId: 1,
+};
+const ticketOrder = {
+  userId: 1,
+  eventId: 1,
+};
+
+//These objects are necessary to fill RightCartSummary and for regarding actions. 
+//It should not be forgotten that they are not exactly body values which are expected by API.
+//It can be checked on the Swagger by going to the endpoints starting with /api/v1.0/orders to see all required fields.
