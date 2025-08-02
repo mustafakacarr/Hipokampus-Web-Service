fa635028f48692ebbe2eb91139e361bfc4428204
dezeve <dezeve@outlook.com>
2025-08-03 01:26:27 +0300

    feat: update dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import UserPanelLayout from "../layouts/UserPanelLayout";
+import { NavLink } from "react-router-dom";
 
 const Dashboard = () => {
   return (
@@ -9,10 +10,13 @@ const Dashboard = () => {
           <div className="container-fluid p-5 dashboard-jumbotron rounded-3">
             <h1 className="display-6 fw-bold">InWork Advantages</h1>
             <p className="col-md-8 fs-4">
-              Lorem ipsum dolor sit amet consectetur adipiscing elit.
-              Quisque faucibus ex sapien vitae pellentesque sem placerat.
+              Enjoy offices with top-notch facilities,
+              a variety of advantages and an environment
+              that meets your needs with InWork.
             </p>
-            <button className="btn btn-primary btn-lg fw-bold" type="button">Explore Now</button>
+            <NavLink to={"/packages"}>
+              <button className="btn btn-primary btn-lg fw-bold" type="button">Explore Now</button>
+            </NavLink>
           </div>
         </div>
         <div className="row align-items-md-stretch">
@@ -20,22 +24,27 @@ const Dashboard = () => {
             <div className="h-100 p-5 secondary-dashboard-jumbotron rounded-3">
               <h2 className="fw-bold text-primary">Air-conditioned Offices</h2>
               <p>
-                Vestibulum eu mattis libero, at euismod nulla.
-                Nulla tempus facilisis est quis congue. Nullam non lacinia velit.
-                Sed sed nisl augue. Cras eu consectetur eros.
+                You can work comfortably and increase your
+                productivity in air-conditioned offices at InWork.
+                Check out our offices to enjoy working in comfort.
               </p>
-              <button className="btn btn-primary fw-bold" type="button">Feel Refreshed</button>
+              <NavLink to={"/packages"}>
+                <button className="btn btn-primary fw-bold" type="button">Feel Refreshed</button>
+              </NavLink>
             </div>
           </div>
           <div className="col-md-6 py-4 py-md-0">
             <div className="h-100 p-5 dashboard-jumbotron rounded-3">
-              <h2 className="fw-bold">New Ideas</h2>
+              <h2 className="fw-bold">Grab Your Coffee</h2>
               <p>
-                Morbi ac purus sapien. Sed ornare, est quis feugiat dictum, enim lorem finibus justo,
-                eu hendrerit est est et diam. Morbi id rutrum felis. Sed elementum vel neque et convallis.
-                Proin a molestie justo, ac fringilla dui.
+                With a wide range of products,
+                you can take advantage of our cafe and
+                benefit from our services while focusing
+                on your business.
               </p>
-              <button className="btn btn-outline-primary fw-bold" type="button">Get Ideas</button>
+              <NavLink to={"/cafe"}>
+                <button className="btn btn-outline-primary fw-bold" type="button">Explore Cafe</button>
+              </NavLink>
             </div>
           </div>
         </div>
