09930d802c0dc552bf8c3b109e019f75591e95e4
dezeve <dezeve@outlook.com>
2025-08-03 21:35:03 +0300

    feat: update subscriptions page

--- src/pages/Subscriptions.jsx
+++ src/pages/Subscriptions.jsx
@@ -1,10 +1,70 @@
 import React from "react";
 import UserPanelLayout from "../layouts/UserPanelLayout";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBriefcase, faFaceAngry, faHandshake, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 const Subscriptions = () => {
   return (
     <UserPanelLayout>
-        <h1>Subscriptions</h1>
+      <div className="row align-items-md-stretch pb-3 pe-1 pe-sm-2 ps-sm-1">
+        <div className="col-md-6 p-2">
+          <div className="h-100 subscriptions-jumbotron p-5 my-3 my-md-0 border rounded-3">
+            <h3 className="fw-bold">
+              <FontAwesomeIcon className="me-2 text-primary" icon={faBriefcase} />
+              Virtual Office
+            </h3>
+            <p className="pt-3 pb-2">
+              Start Date: 14/8/2024
+              <br />
+              End Date: 12/7/2025
+              <br />
+              Corporate Name: OrderFast
+              <br />
+              Tax Office: Lorem
+              <br />
+              Tax Number: 123123
+              <br />
+              Corporate Address: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore laborum consectetur quidem perferendis, LOREM/IPSUM
+            </p>
+            <button className="btn btn-outline-primary" type="button">Operations</button>
+          </div>
+        </div>
+        <div className="col-md-6 p-2">
+          <div className="h-100 subscriptions-jumbotron p-5 my-3 my-md-0 border rounded-3">
+            <h3 className="fw-bold">
+              <FontAwesomeIcon className="me-2 text-primary" icon={faUserGroup} />
+              Coworking Space
+            </h3>
+            <p className="pt-3 pb-2">
+              Start Date: 14/8/2024
+              <br />
+              End Date: 12/7/2025
+              <br />
+              Remained Usage Right: 4
+            </p>
+            <button className="btn btn-outline-primary" type="button">Operations</button>
+          </div>
+        </div>
+        <div className="col-md-6 p-2">
+          <div className="h-100 subscriptions-jumbotron p-5 my-3 my-md-0 border rounded-3">
+            <h3 className="fw-bold">
+              <FontAwesomeIcon className="me-2 text-primary" icon={faHandshake} />
+              Meeting Room
+            </h3>
+            <p className="pt-3 pb-2">
+              Start Date: 14/8/2024
+              <br />
+              End Date: 12/7/2025
+              <br />
+              Corporate Name: OrderFast
+              <br />
+              Office Id: 456456
+              <br />
+            </p>
+            <button className="btn btn-outline-primary" type="button">Operations</button>
+          </div>
+        </div>
+      </div>
     </UserPanelLayout>
   );
 };
